656baa15583555773a35021d00b4a6fb42ad881f
shakiroh55 <fhatshakirat@gmail.com>
2023-10-18 15:23:25 +0100

    new commit by shakiroh

--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -2,10 +2,66 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to KJMAirline Website</h1>
-            <p>Hello world</p>
-        </div>
+        <>
+            <div className='IFE'>
+
+            </div>
+            <div className="SHAKIROH overflow-x-auto flex justify-between">
+                <table className="table">
+                    {/* head */}
+                    {/* <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead> */}
+                    <tbody>
+                        <tr>
+                        <td>Cy Ganderton</td>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        <tr>
+                        <td>Cy Ganderton</td>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        <tr>
+                        <td>Cy Ganderton</td>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+
+                        <tr>
+                        <td>Cy Ganderton</td>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        <tr>
+                        <td>Cy Ganderton</td>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        <tr>
+                        <td>Cy Ganderton</td>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='BOSS'>
+
+            </div>
+        </>
     )
 }
 

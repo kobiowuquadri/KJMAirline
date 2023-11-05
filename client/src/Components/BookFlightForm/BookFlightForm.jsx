13391450d21432e7ff139905e3758dd99920ee78
Engr_ifex <ayobamiadesoji2@gmail.com>
2023-11-05 15:04:22 -0800

    countries

--- client/src/Components/BookFlightForm/BookFlightForm.jsx
+++ client/src/Components/BookFlightForm/BookFlightForm.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import './bookfight.scss'
 import Payment from '../Payment/Payment'
 
 function BookFlightForm () {
+
+  const [country, setCountry]= useState([]);
+  const [countryid, setCountryid]= useState('');
+
+  useEffect( ()=>{
+   const getcountry= async ()=>{
+     const req= await fetch("https://restcountries.eu/rest/v2/all");
+     const getres= await req.json();
+     console.log(getres);
+     setCountry(await getres);
+
+   }
+   getcountry();
+
+
+  },[]);
+
+  const handlecountry=(event)=>{
+    const getcoutryid= event.target.value;
+    setCountryid(getcoutryid);
+    event.preventDefault();
+  }
+
   return (
     <div className='bg_flight'>
       <h1 className='text-white fw-bold p-4 fs-1'>Book a Flight</h1>
@@ -24,22 +47,38 @@ function BookFlightForm () {
           </div>
           <div class='form-group d-sm-flex margin'>
             <div class='d-flex align-items-center flex-fill me-sm-1 my-sm-0 my-4 border-bottom position-relative'>
-              <input
+              {/* <input
                 type='text'
                 required
                 placeholder='From'
                 class='form-control'
-              />
+              /> */}
+
+              <select className='form-control'>
+                   <option selected>From</option>
+                   <option>America</option>
+                 
+                 </select>
               <div class='label' id='from'></div>
               <span class='fas fa-dot-circle text-muted'></span>
             </div>
             <div class='d-flex align-items-center flex-fill ms-sm-1 my-sm-0 my-4 border-bottom position-relative'>
-              <input
+              {/* <input
                 type='text'
                 required
                 placeholder='To'
                 class='form-control'
-              />
+              /> */}
+
+                <select name="country" className="form-control" onChange={(e)=>handlecountry(e)}>
+                   <option selected>To</option>
+                   {
+                     country.map( (getcon)=>(
+                   <option key={getcon.country_id} value={getcon.country_id }> { getcon.country_name}</option>
+                     ))
+                }
+                 
+                 </select>
               <div class='label' id='to'></div>
               <span class='fas fa-map-marker text-muted'></span>
             </div>
@@ -65,12 +104,25 @@ function BookFlightForm () {
             </div>
           </div>
           <div class='form-group border-bottom d-flex align-items-center position-relative'>
-            <input
+            {/* <input
               type='text'
               required
               placeholder='Traveller(s)'
               class='form-control'
-            />
+            /> */}
+
+            <select className='form-control'>
+                   <option selected>1</option>
+                   <option>2</option>
+                   <option>3</option>
+                   <option>4</option>
+                   <option>5</option>
+                   <option>6</option>
+                   <option>7</option>
+                   <option>8</option>
+                   <option>9</option>
+                   <option>10</option>
+                 </select>
             <div class='label' id='psngr'></div>
             <span class='fas fa-users text-muted'></span>
           </div>

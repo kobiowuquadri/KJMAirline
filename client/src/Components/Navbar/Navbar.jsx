76d79ad42b8770da898fd4a0c27f852971037768
kobiowuquadri <kobiowuq@gmail.com>
2023-11-08 08:44:16 -0600

    update home page responsiveness

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,60 +1,103 @@
-import React, { useContext } from 'react';
-import usa from '../../Assets/Images/usa_logo.png';
-import logo from '../../Assets/Images/kjm-logo.png';
-import { CgProfile } from 'react-icons/cg';
-import { MdDirectionsWalk } from 'react-icons/md';
-import { Link } from 'react-router-dom';
-import './navbar.scss';
-import { AuthContext } from '../../contexts/authContext';
+import React, { useContext, useState } from 'react'
+import usa from '../../Assets/Images/usa_logo.png'
+import logo from '../../Assets/Images/kjm-logo.png'
+import { CgProfile } from 'react-icons/cg'
+import { MdDirectionsWalk } from 'react-icons/md'
+import { Link } from 'react-router-dom'
+import './navbar.scss'
+import { AuthContext } from '../../contexts/authContext'
+import close_menu_black from '../../Assets/Images/icon-close_white.svg'
+import hamburger from '../../Assets/Images/icon-menu.svg'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
-function Navbar() {
-  const { userDatas, isUser, logout } = useContext(AuthContext); // Access the isUser state from the context
+function Navbar () {
+  const { userDatas, isUser, logout } = useContext(AuthContext)
+  let [toggle, updateToggle] = useState(false)
 
   const handleLogout = () => {
-    logout();
-  };
+    logout()
+  }
 
   return (
-    <div id="navhero">
-      <div id="navtop">
-        <div>
-          <img src={logo} alt="Logo" id="logo" />
-        </div>
-        <div id="navtopside">
-          <img src={usa} alt="Nigeria flag" id="navtopsideimg" />
-          <p className="pe-4">America - US</p>
+    <>
+      <div id='navcard' className={toggle && 'show'}>
+        <img
+          id='close'
+          onClick={() => updateToggle(false)}
+          width={'20px'}
+          src={close_menu_black}
+        ></img>
+        <img width={'80px'} src={usa} alt='Nigeria flag' />
+        <p className='text-white'>America - US</p>
+        <Link to='/'>Home</Link>
+        <Link to='/mytrip'>My Trip</Link>
+        <Link to='/information'>Information</Link>
 
-          <div id="navtopsidecontent">
-            {isUser ? (
-              <>
-                {userDatas && <p className="pe-4">Welcome, {userDatas.username}</p>} 
-                <CgProfile id="navtopsideimg" />
-                <button id="navtoplink" onClick={handleLogout}>
-                  Logout
-                </button>
-              </>
-            ) : (
-              <div id="navtopsidecontent">
-                <CgProfile id="navtopsideimg" />
-                <Link to="/login" id="navtoplink">
-                  <p>Log in</p>
-                </Link>
-                <MdDirectionsWalk id="navtopsideimg" />
-                <Link to="/signup" id="navtoplink">
-                  <p>Sign Up</p>
-                </Link>
-              </div>
+        {isUser ? (
+          <>
+            {userDatas && (
+              <p className='text-white'>Welcome, {userDatas.username}</p>
             )}
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <div>
+            <Link to='/login' id='navtoplink'>
+              <p>Log in</p>
+            </Link>
+            <MdDirectionsWalk id='navtopsideimg' />
+            <Link to='/signup' id='navtoplink'>
+              <p>Sign Up</p>
+            </Link>
           </div>
+        )}
+      </div>
+      <div id='navhero'>
+        <div id='navtop'>
+          <div>
+            <img src={logo} alt='Logo' id='logo' />
+          </div>
+          <div id='navtopside'>
+            <img src={usa} alt='Nigeria flag' id='navtopsideimg' />
+            <p className='pe-4'>America - US</p>
+
+            <div id='navtopsidecontent'>
+              {isUser ? (
+                <>
+                  {userDatas && (
+                    <p className='pe-4'>Welcome, {userDatas.username}</p>
+                  )}
+                  <CgProfile id='navtopsideimg' />
+                  <button id='navtoplink' onClick={handleLogout}>
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <div id='navtopsidecontent'>
+                  <CgProfile id='navtopsideimg' />
+                  <Link to='/login' id='navtoplink'>
+                    <p>Log in</p>
+                  </Link>
+                  <MdDirectionsWalk id='navtopsideimg' />
+                  <Link to='/signup' id='navtoplink'>
+                    <p>Sign Up</p>
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+          <p id='hamburger' onClick={() => updateToggle(true)}>
+            {<RxHamburgerMenu />}
+          </p>
+        </div>
+        <div id='navbottom'>
+          <Link to='/'>Home</Link>
+          <Link to='/mytrip'>My Trip</Link>
+          <Link to='/information'>Information</Link>
         </div>
       </div>
-      <div id="navbottom">
-        <Link to="/">Home</Link>
-        <Link to="/mytrip">My Trip</Link>
-        <Link to="/information">Information</Link>
-      </div>
-    </div>
-  );
+    </>
+  )
 }
 
-export default Navbar;
+export default Navbar
--- client/src/Pages/BookFlight/BookFlight.jsx
+++ client/src/Pages/BookFlight/BookFlight.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import BookFlightForm from '../../Components/BookFlightForm/BookFlightForm'
+import Footer from '../../Components/Footer/Footer'
 
 function BookFlight () {
   return (
     <>
       <Navbar />
       <BookFlightForm/>
+      <Footer/>
     </>
   )
 }

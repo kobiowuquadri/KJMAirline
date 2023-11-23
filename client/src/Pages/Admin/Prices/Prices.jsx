b764cdfe3e4f2d9cb6937bdce2c11cd0aa54f906
kobiowuquadri <kobiowuq@gmail.com>
2023-11-23 07:08:58 -0600

    add admin price componenet

--- client/src/Pages/Admin/Prices/Prices.jsx
+++ client/src/Pages/Admin/Prices/Prices.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Slidebar from '../../../Components/SideBar/SideBar'
 import axios from 'axios'
 import './prices.scss'
@@ -6,14 +6,18 @@ import './prices.scss'
 
 
 function Prices() {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
 
-  const handleCreatePrice = async() => {
+  const handleCreatePrice = async () => {
     try {
       const response = await axios.post('https://kjm.zuuroo.com/api/admin/create_price', {
-       
+       name,
+       price
       })
-    } catch (error) {
-      console.log(response?.data?.error)
+      console.log(response?.data)
+    } catch (err) {
+      console.log(err?.response?.data)
     }
   }
 
@@ -22,18 +26,18 @@ function Prices() {
     <div className='admin__dashbaord'>
     <Slidebar/>
    <div className='main__admin2 shadow mt-2'>
-    <h1>Update Prices</h1>
-    <form action="" id='update__prices' className='shadow'>
+    <h1>Create Prices</h1>
+    <form onSubmit={handleCreatePrice} id='update__prices' className='shadow'>
      <p>
      <label for="" >Flight Name: </label>
-      <input type="text" />
+      <input type="text" name='name' value={name} onChange={(e) => setName(e.target.value)} />
      </p>
      <p>
      <label for="" >Amount: </label>
-      <input type="number" />
+      <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} name='price' />
      </p>
      <p>
-      <input type="submit" className='btn btn-primary border-0' value="Update Price" />
+      <input type="submit"  className='btn btn-primary border-0' value="Update Price" />
      </p>
     </form>
     </div>

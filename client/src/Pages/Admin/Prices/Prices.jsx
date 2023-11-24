66b730157a9b72bfa6ebc1f59c3bde520a104ce2
kobiowuquadri <kobiowuq@gmail.com>
2023-11-24 12:30:56 -0600

    add all prices

--- client/src/Pages/Admin/Prices/Prices.jsx
+++ client/src/Pages/Admin/Prices/Prices.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
 import Slidebar from '../../../Components/SideBar/SideBar'
 import axios from 'axios'
 import './prices.scss'
 
 function Prices () {
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-  const [id, setId] = useState('')
-  const [flightPrices, getFlightPrices] = useState([])
+  const [flightOptions, setFlightOptions] = useState([]);
+  const [selectedFlight, setSelectedFlight] = useState(null);
+  const [selectedId, setSelectedId] = useState('');
+  const [price, setPrice] = useState('');
+  const [flightPrices, getFlightPrices] = useState([]);
 
   const adminAccessToken = localStorage.getItem('adminAccessToken')
+
   const handleUpdatePrice = async e => {
     e.preventDefault()
     try {
       const response = await axios.post(
         'https://kjm.zuuroo.com/api/admin/update_price',
         {
-          name,
+         name: selectedFlight?.value,
           price,
-          id
+          id: selectedId,
         },
         {
           headers: {
@@ -50,7 +53,15 @@ function Prices () {
       console.log(response?.data)
       const { data } = response?.data
       getFlightPrices(data)
-      console.log(data)
+
+      // Create options for flight names
+      const options = data.map((flight) => ({
+        value: flight.flight_name,
+        label: flight.flight_name,
+        id: flight.id,
+      }));
+
+      setFlightOptions(options);
     } catch (err) {
       console.log(err?.response?.data)
     }
@@ -60,57 +71,21 @@ function Prices () {
     showAllFlightPrices()
   }, [])
 
+  const handleFlightChange = (selectedOption) => {
+    setSelectedFlight(selectedOption);
+    setSelectedId(selectedOption?.id || '');
+  };
+
   return (
     <div className='admin__dashbaord'>
       <Slidebar />
       <div className='main__admin2 shadow mt-2'>
-        <h1>Update Price</h1>
-        <form
-          onSubmit={handleUpdatePrice}
-          id='update__prices'
-          className='shadow'
-        >
-          <p>
-            <label for=''>Flight Name: </label>
-            <input
-              type='text'
-              name='name'
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
-          </p>
-          <p>
-            <label for=''>Amount: </label>
-            <input
-              type='number'
-              value={price}
-              onChange={e => setPrice(e.target.value)}
-              name='price'
-            />
-          </p>
-          <p>
-            <label for=''>ID: </label>
-            <input
-              type='number'
-              value={id}
-              onChange={e => setId(e.target.value)}
-              name='id'
-            />
-          </p>
-          <p>
-            <input
-              type='submit'
-              className='btn btn-primary border-0'
-              value='Update Price'
-            />
-          </p>
-        </form>
-        <div className='w-100 d-flex justify-content-center p-5 align-items-center min-vh-50 flex-column'>
+      <div className='w-100 d-flex justify-content-center p-5 align-items-center min-vh-50 flex-column'>
           <h1>All Prices</h1>
-          <table width={'50%'}>
+          <table width={'50%'} border={1}>
            <tr className='d-flex justify-content-between align-items-center'>
            <th>Flight Name</th>
-            <th>Price</th>
+            <th>Current Price</th>
             <th>ID</th>
            </tr>
           </table>
@@ -128,6 +103,49 @@ function Prices () {
             )
           })}
         </div>
+
+        <p className='text-center fw-bold bg-primary text-white p-3'>Update Price</p>
+        <form
+          onSubmit={handleUpdatePrice}
+          id='update__prices'
+          className='shadow'
+        >
+          <p>
+          <label htmlFor=''>Flight Name: </label>
+          <Select
+              value={selectedFlight}
+              onChange={handleFlightChange}
+              options={flightOptions}
+            />
+          </p>
+          <p>
+          <label htmlFor=''>New Price: </label>
+            <input
+              type='number'
+              placeholder='Enter new price'
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              name='price'
+            />
+          </p>
+          <p>
+          <label htmlFor=''>ID: </label>
+            <input
+              type='number'
+              value={selectedId}
+              readOnly
+              name='id'
+            />
+          </p>
+          <p>
+            <input
+              type='submit'
+              className='btn btn-primary border-0'
+              value='Update Price'
+            />
+          </p>
+        </form>
+        
       </div>
     </div>
   )
